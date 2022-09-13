09ce8b17da174cac408e05e131127be3b1ab2e23
Tai LE <lhtai181@gmail.com>
2022-09-13 15:22:14 +0700

    update seed.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,13 @@ class User < ActiveRecord::Base
     premium: 'premium'
   }.freeze
 
-  has_many :earning_histories
+  has_many :earning_histories, dependent: :delete_all
   has_many :rewards, through: :user_rewards
-  has_many :transactions
-  has_many :user_rewards
+  has_many :transactions, dependent: :delete_all
+  has_many :user_rewards, dependent: :delete_all
 
   validates :name, :point, :tier, :currency, presence: true
   validates :point, numericality: { greater_than_or_equal_to: 0 }
-  validates :tier, inclusion: { in: TIERS.values, message: "%{value} is not valid" }
-  validates :currency, inclusion: { in: CURRENCIES.values, message: "%{value} is not valid" }
+  validates :tier, inclusion: { in: TIERS.values, message: '%{value} is not valid' }
+  validates :currency, inclusion: { in: CURRENCIES.values, message: '%{value} is not valid' }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# clean up data
+Reward.delete_all
+User.delete_all
+
+# create rewards
+[
+  'Free Coffee Reward',
+  '5% Cash Rebate Reward',
+  'Free Movie Ticket Reward',
+  '4x Airport Lounge Access Reward'
+].each do |name|
+  Reward.create(name: name)
+end
+
+# create users
+User.create!([
+               { name: 'Tony LE', dob: '1981-04-18', tier: User::TIERS[:standard] },
+               { name: 'Wendy LE', dob: '2015-02-05', tier: User::TIERS[:gold] },
+               { name: 'Jacky LE', dob: '2016-11-19', tier: User::TIERS[:premium], currency: User::CURRENCIES[:eur] }
+             ])
